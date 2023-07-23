741cc121d3e1e394437c1b3aa083ed011f1ebb8a
weroszymaniak <weronika.szymaniak@hotmail.com>
2023-07-23 12:18:10 +0200

    changed url in operations

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://64b6c86adf0839c97e161d83.mockapi.io';
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
