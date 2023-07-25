281412af42e47b8b9d93c262d36d3a4508844b00
weroszymaniak <116954355+weroszymaniak@users.noreply.github.com>
2023-07-25 19:18:41 +0000

    Deploying to gh-pages from @ weroszymaniak/goit-react-hw-08-phonebook@a8b3da6df440c2a25e7d72bca8b1cdb8623891b9 🚀

--- static/js/415.16189e07.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var o=n(184),c={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function i(){return(0,o.jsx)("div",{style:c.container,children:(0,o.jsxs)("h1",{style:c.title,children:["Welcome to your phonebook, log in to see your contacts"," "]})})}}}]);
-//# sourceMappingURL=415.16189e07.chunk.js.map
